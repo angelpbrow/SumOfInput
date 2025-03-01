874536aae358dffed53b5e1bc30cd99de5e83b7d
Angelika Brown <angelikpbrown@gmail.com>
2025-02-28 22:38:02 -0500

    almost done

---
+++ SumOfInput.java
@@ -0,0 +1,17 @@
+public class SumOfInput
+public static void main (String[] args) {
+string line;
+  Scanner in = new Scanner(System.in);
+    System.out.print("Enter a number: ")
+
+  int sumNumbers(int n) {
+    int sum = 0;
+    for (int i = 1; i <= n; i++) {
+        sum = sum + 1;
+    }
+    return sum;
+}
+}  
+
+}
+
