9e7960fe992df44248f6ae7c761685fb492fcc51
Angelika Brown <angelikpbrown@gmail.com>
2025-03-01 19:07:54 -0500

    Update SumOfInput.java

--- SumOfInput.java
+++ SumOfInput.java
@@ -1,10 +1,12 @@
-public class SumOfInput
-public static void main (String[] args) {
-string line;
-  Scanner in = new Scanner(System.in);
-    System.out.print("Enter a number: ")
+import java.util.Scanner;
 
-  int sumNumbers(int n) {
+public class SumOfInput {
+   public static void main (String[] args) {
+     Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter a number: ");
+     int n = scanner.nextInt();
+     
+    int sumNumbers(int n) {
     int sum = 0;
     for (int i = 1; i <= n; i++) {
         sum = sum + 1;
